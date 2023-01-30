730da3493f6160f78010d82944b9cdf76d718c13
waiga <hyang9387@gmail.com>
2023-01-30 22:59:02 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email
+from secret_key import MY_SECRET_KEY
+
+
+class LoginForm(FlaskForm):
+    email = StringField(label='Email', validators=[DataRequired(message='email입력'), Email()])
+    password = PasswordField(label='Password', validators=[DataRequired(message='password입력')])
+    submit = SubmitField(label="Log In")
+
+
+app = Flask(__name__)
+app.secret_key = MY_SECRET_KEY
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    login_form = LoginForm()
+    print(login_form)
+    login_form.validate_on_submit()
+    return render_template('login.html', form=login_form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
